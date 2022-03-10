b0ec7f90174274faff79e5a1ebddb130d125d818
soph <annasarcangel@gmail.com>
2022-03-09 22:35:41 -0800

    Test

---
+++ app/src/main/java/adacat/com/carcassonneunittest/MainActivity.java
@@ -0,0 +1,23 @@
+package adacat.com.carcassonneunittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+    }
+}
